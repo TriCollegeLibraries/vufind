d54e4d45b044531446558fd4f6c91955d5e681e6
Anna Headley <anna3LC@gmail.com>
2015-01-23 11:45:47 -0500

    Configure alphabrowse to use new dewey normalizer; fix quotes in .bat script"

--- index-alphabetic-browse.sh
+++ index-alphabetic-browse.sh
@@ -71,4 +71,4 @@ build_browse "title" "title_fullStr" 1 "-Dbibleech=StoredFieldLeech -Dsortfield=
 build_browse "topic" "topic_browse"
 build_browse "author" "author_browse"
 build_browse "lcc" "callnumber-a" 1 "-Dbrowse.normalizer=org.vufind.util.LCCallNormalizer"
-build_browse "dewey" "dewey-raw" 1 "-Dbibleech=StoredFieldLeech -Dsortfield=dewey-sort-browse -Dvaluefield=dewey-raw"
+build_browse "dewey" "dewey-raw" 1 "-Dbibleech=StoredFieldLeech -Dsortfield=dewey-sort-browse -Dvaluefield=dewey-raw -Dbrowse.normalizer=org.vufind.util.DeweyCallNormalizer"
